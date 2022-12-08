101eef58f66640cc88b82abc16c6d2235466f53d
PAC lab <rodgers.paclab@gmail.com>
2022-12-08 12:18:55 -0500

    jellyfish nightly rsync script

---
+++ jellyfish_nightly_rsync/jellyfish_nightly_rsync.py
@@ -0,0 +1,42 @@
+# rsync the video data from jellyfish to cuttlefish
+#
+# Run like this:
+# /bin/bash -l -c 'cd /home/mouse/dev/autoscripts/jellyfish_nightly_rsync; python jellyfish_nightly_rsync.py >> logfile 2>&1'
+# This ensures that the python installation and paths are correct
+
+# Announce the start and start time
+print("---------------")
+print("AUTORUN_START__")
+import datetime
+import os
+print("AUTORUN_START_TIME : %s" % str(datetime.datetime.now()))
+print("AUTORUN_START_FILE {}".format(os.path.abspath(__file__)))
+
+# Rest of imports
+import sys
+
+# rsync will be called with this logfile
+logfile = 'nightly_rsync.log'
+
+# rsync -va /media/mouse/windows/Users/mouse/Videos /home/mouse/mnt/cuttlefish/behavior/videos/e3vision
+# Check that it's actually mounted before running
+check_cuttlefish_mount_dir = '/home/mouse/mnt/cuttlefish'
+if not os.path.ismount(check_cuttlefish_mount_dir):
+    raise IOError("This must be mounted: %s" % check_cuttlefish_mount_dir)
+
+# This is where output goes
+output_dir = os.path.join(
+    check_cuttlefish_mount_dir, 'behavior', 'videos', 'e3vision')
+
+# Copy behavior
+input_dir = '/home/mouse/Videos'
+cmd = 'rsync -va --log-file=%s %s %s' % (
+    logfile, input_dir, output_dir)
+print("rsync : %s" % str(datetime.datetime.now()))
+print(cmd)
+sys.stdout.flush() # to maintain the order of print and os.system in the logfile
+os.system(cmd)
+
+# Print stop time
+print("AUTORUN_STOP_TIME : %s" % str(datetime.datetime.now()))
+print("AUTORUN_STOP__")
