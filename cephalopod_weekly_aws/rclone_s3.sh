5f4f60e32d4ce71cc8a914a1d9407a4d78b90dae
Chris Rodgers <xrodgers@gmail.com>
2023-03-04 16:02:16 -0500

    fast-list; log-level INFO; adding lucas

--- cephalopod_weekly_aws/rclone_s3.sh
+++ cephalopod_weekly_aws/rclone_s3.sh
@@ -1,14 +1,15 @@
 echo AUTORUN_START__
 echo AUTORUN_START_TIME : `date +%F_%H-%M-%S`
 echo AUTORUN_SCRIPT : $(realpath "$0")
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/abigail remote:emory-rodgerslab-backup-hot-20221205/abigail
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/behavior remote:emory-rodgerslab-backup-hot-20221205/behavior
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/carrissa remote:emory-rodgerslab-backup-hot-20221205/carrissa
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/cedric remote:emory-rodgerslab-backup-hot-20221205/cedric
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/chris remote:emory-rodgerslab-backup-hot-20221205/chris
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/devinenilab remote:emory-rodgerslab-backup-hot-20221205/devinenilab
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/osama remote:emory-rodgerslab-backup-hot-20221205/osama
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/rowan remote:emory-rodgerslab-backup-hot-20221205/rowan
-rclone copy --log-file=rclone.log ~/mnt/cuttlefish/whitematter remote:emory-rodgerslab-backup-hot-20221205/whitematter
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/abigail remote:emory-rodgerslab-backup-hot-20221205/abigail
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/behavior remote:emory-rodgerslab-backup-hot-20221205/behavior
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/carrissa remote:emory-rodgerslab-backup-hot-20221205/carrissa
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/cedric remote:emory-rodgerslab-backup-hot-20221205/cedric
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/chris remote:emory-rodgerslab-backup-hot-20221205/chris
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/devinenilab remote:emory-rodgerslab-backup-hot-20221205/devinenilab
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/lucas remote:emory-rodgerslab-backup-hot-20221205/lucas
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/osama remote:emory-rodgerslab-backup-hot-20221205/osama
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/rowan remote:emory-rodgerslab-backup-hot-20221205/rowan
+rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/whitematter remote:emory-rodgerslab-backup-hot-20221205/whitematter
 echo AUTORUN_STOP_TIME `date +%F_%H-%M-%S`
 echo AUTORUN_STOP__
