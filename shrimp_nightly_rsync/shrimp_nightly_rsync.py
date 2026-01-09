d528bcfd0cbbd3e957b30b8ab4f77fad09a1d57d
PAC Lab <rodgers.paclab@gmail.com>
2026-01-09 17:33:43 -0500

    updated shrimp rsync; now includes open-ephys

--- shrimp_nightly_rsync/shrimp_nightly_rsync.py
+++ shrimp_nightly_rsync/shrimp_nightly_rsync.py
@@ -1,10 +1,14 @@
-# rsync the video data from shrimp to cuttlefish
+# rsync data from shrimp to cuttlefish
+# * ~/open-ephys
+# * ~/octopilot
+# * ~/Videos
 #
 # Run like this:
 # /bin/bash -l -c 'cd /home/mouse/dev/autoscripts/shrimp_nightly_rsync; python3 shrimp_nightly_rsync.py >> logfile 2>&1'
 # This ensures that the python installation and paths are correct
 
-# Announce the start and start time
+
+## Announce the start and start time
 print("---------------")
 print("AUTORUN_START__")
 import datetime
@@ -15,37 +19,58 @@ print("AUTORUN_START_FILE {}".format(os.path.abspath(__file__)))
 # Rest of imports
 import sys
 
+
+## Logfile
 # rsync will be called with this logfile
 logfile = 'nightly_rsync.log'
 
-# rsync -va /media/mouse/windows/Users/mouse/Videos /home/mouse/mnt/cuttlefish/behavior/videos/e3vision
-# Check that it's actually mounted before running
+
+## Check that output diretories are actually mounted before running
 check_cuttlefish_mount_dir = '/home/mouse/mnt/shrimpX'
 if not os.path.ismount(check_cuttlefish_mount_dir):
     raise IOError("This must be mounted: %s" % check_cuttlefish_mount_dir)
 
-# This is where output goes
-output_dir = check_cuttlefish_mount_dir
+check_cuttlefish_mount_dir = '/home/mouse/mnt/cuttlefish'
+if not os.path.ismount(check_cuttlefish_mount_dir):
+    raise IOError("This must be mounted: %s" % check_cuttlefish_mount_dir)
 
-# Copy videos
-input_dir = '/home/mouse/Videos'
-cmd = 'rsync -va --no-p --log-file=%s %s %s' % (
-    logfile, input_dir, output_dir)
-print("rsync : %s" % str(datetime.datetime.now()))
-print(cmd)
-sys.stdout.flush() # to maintain the order of print and os.system in the logfile
-os.system(cmd)
 
-# Copy behavior
+## The reusable copy function
+def run_rsync(input_dir, output_dir):
+    # Form the command
+    cmd = 'rsync -va --no-p --log-file=%s %s %s' % (
+        logfile, input_dir, output_dir)
+    
+    # Log the command
+    print("rsync : %s" % str(datetime.datetime.now()))
+    print(cmd)
+    
+    # This maintains the order of print and os.system in the logfile
+    sys.stdout.flush() 
+    
+    # Run the command
+    os.system(cmd)    
+
+
+## Copy ~/open-ephys to ~/mnt/cuttlefish/shrimp
 input_dir = '/home/mouse/octopilot'
 output_dir = '/home/mouse/mnt/cuttlefish/shrimp'
-cmd = 'rsync -va --no-p --log-file=%s %s %s' % (
-    logfile, input_dir, output_dir)
-print("rsync : %s" % str(datetime.datetime.now()))
-print(cmd)
-sys.stdout.flush() # to maintain the order of print and os.system in the logfile
-os.system(cmd)
+run_rsync(input_dir, output_dir)
 
-# Print stop time
+
+## Copy ~/Videos to shrimpX
+input_dir = '/home/mouse/Videos'
+output_dir = '/home/mouse/mnt/shrimpX'
+run_rsync(input_dir, output_dir)
+
+
+## Copy ~/octopilot to ~/mnt/cuttlefish/shrimp
+input_dir = '/home/mouse/octopilot'
+output_dir = '/home/mouse/mnt/cuttlefish/shrimp'
+run_rsync(input_dir, output_dir)
+
+
+## Print stop time
+print()
 print("AUTORUN_STOP_TIME : %s" % str(datetime.datetime.now()))
 print("AUTORUN_STOP__")
