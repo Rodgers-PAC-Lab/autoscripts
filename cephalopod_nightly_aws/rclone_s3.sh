4026fba7b9bef2591b044e256a1fad79814309bc
Chris Rodgers <xrodgers@gmail.com>
2026-04-19 21:06:15 -0400

    add stats-one-line and multi-thread-streams

--- cephalopod_nightly_aws/rclone_s3.sh
+++ cephalopod_nightly_aws/rclone_s3.sh
@@ -2,6 +2,6 @@
 echo AUTORUN_START__
 echo AUTORUN_START_TIME : `date +%F_%H-%M-%S`
 echo AUTORUN_SCRIPT : $(realpath "$0")
-rclone copy --fast-list --log-file=rclone.log --log-level INFO ~/mnt/cuttlefish/whitematter remote:emory-rodgerslab-backup-hot-20221205/whitematter
+rclone copy --fast-list --log-file=rclone.log --log-level INFO --stats-one-line --multi-thread-streams 0 ~/mnt/cuttlefish/whitematter remote:emory-rodgerslab-backup-hot-20221205/whitematter
 echo AUTORUN_STOP_TIME `date +%F_%H-%M-%S`
 echo AUTORUN_STOP__
